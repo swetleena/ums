cda2c9fd44e628bfeaede031ad654a901a7e8688
gurjeet_si <gurjeet_si@hcl.com>
2018-07-16 14:41:38 +0530

    my first project

--- userManagement.js
+++ userManagement.js
@@ -7,25 +7,40 @@ app.use(bodyparser.json());
 
 global.USERS = [];
 
-
+// To get Home page
 app.get('/', function(req, res) {
     res.sendFile(__dirname + "/"+"home.html");
 });
 
+// To get user registration page after clicking on "User registration" button on home page
 app.get('/regPage',(req,res)=>{
-	// console.log(__dirname + "/"+"regPage.html")
     res.sendFile(__dirname + "/"+"regPage.html");
 });
 
-
+// To get all the user list to manage users after clicking on "User list management" button on home page
 app.get('/userList',(req,res)=>{
 
- var doc = "<html><head><title>All Users</title></head><body>";
+ var userDelete= "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>\n";
+      userDelete+= "<script>function doDelete(eid){\n";
+      userDelete+=   "$.ajax({\n";
+      userDelete+=   "type: 'DELETE',\n";
+      userDelete+=   "url: \"http://localhost:3000/userList/\"+eid+\"/delete\",\n";
+      userDelete+=   "data: {eid: eid},\n";
+      userDelete+=   "success: function(data){\n";
+      userDelete+=   "alert('Deleted Sucessfully');\n";
+      userDelete+=   "location.reload();\n";
+      userDelete+=   " },\n";
+      userDelete+=   "error: function(jqXHR, textStatus, errorThrown) {\n";
+      userDelete+=   "alert('Some problem while deleting')\n"
+      userDelete+=   "}});}</script>";
+
+
+ var doc = "<html><head><title>All Users</title> "+userDelete+"</head><body>";
  var table = "<table>"
- var tableHeader = "<thead><th>UserName</th><th>eMailId</th><th>DOB</th><th>Mobile</th><th>Gender</th><th>Actions</th></thead>";
+ var tableHeader = "<thead><th>UserName</th><th>eMailId</th><th>DOB</th><th>Mobile</th><th>Actions</th></thead>";
  var tableBody = "<tbody>";
  USERS.forEach(function(e){
-                             var row="<form  method='post'><tr><td>"+e.username+"</td><td>"+e.email+"</td><td>"+e.dob+"</td><td>"+e.mobile+"</td><td><input type=\"button\" value=\"Edit\" onclick=\"window.location.href='http://localhost:3000/userList/edit'\"/> <button>Delete</button></td></tr>";
+                             var row="<tr><td>"+e.username+"</td>\n<td>"+e.email+"</td>\n<td>"+e.dob+"</td>\n<td>"+e.mobile+"</td>\n<td><button onclick=\"window.location.href='http://localhost:3000/userList/"+e.email+"'\"/>Edit</button>\n<button onclick=\"doDelete('"+e.email+"')\">Delete</button></td>\n</tr>\n";
  
                              tableBody=tableBody+row;
  
@@ -37,71 +52,95 @@ app.get('/userList',(req,res)=>{
 
 });
 
+// To get user's profile form to edit as a result of clicking on "edit" button to update user details
+app.get('/userList/:eid',(req,res)=>{
+var header= "<html><head><title>User Profile</title></head><body><h1>User Profile </h1>";
+var userEmail = req.params.eid;
+var form ="<form action ='/userlist/"+userEmail+"/update' method='post'>";
+USERS.forEach(function(e){
 
-app.get('/userList/edit',(req,res)=>{
+  if(userEmail == e.email)
+  {
+ form+= "<div style='float:left'><table>";
+ form+="<tr><td>Name:</td><td><input type='text' name='username' value ='"+ e.username +"'/></td></tr>";
+ form+="<tr><td>Email:</td><td><input type='email' name='email' value = '"+e.email+"' disable/></td></tr>";
+ form+="<tr><td>DOB:</td><td><input type='text' name='dob' value = '"+e.dob+"'/></td></tr>";
+ form+="<tr><td>Mobile:</td><td><input type='text' name='mobile' value = '"+e.mobile+"'/></td></tr>";
+ form+="</table></div>";
+ form+="<div><img height ='100' width='100'/><br/><input type=' file' name='dp'/></div>";
+ form+="<input type='submit' value='update'/></body></html>";
 
- var doc = "<html><head><title>All Users</title></head><body>";
- var table = "<table>"
- var tableHeader = "<thead><th>UserName</th><th>eMailId</th><th>DOB</th><th>Mobile</th><th>Gender</th><th>Actions</th></thead>";
- var tableBody = "<tbody>";
- USERS.forEach(function(e){
-                             var row="<form action='/userList/update' method='post'><tr><td><input type='text' name='username'  value='"+e.username+"'/></td><td><input type='text' name='email' value='"+e.email+"'/></td><td><input type='text' name='dob' value='"+e.dob+"'/></td><td><input type='text' name='mobile' value='"+e.mobile+"'/></td><td><input type='text' name='gender' value='"+e.gender+"'></td><td><input type='submit' value='Update'/> <button>Delete</button></td></tr>";
- 
-                             
- 
-   });
- tableBody=tableBody+row;
- tableBody+="</tbody>";
- table = table+tableHeader+tableBody+"</table>";
- doc = doc+table+"</body>" + "</html>";
- res.send(doc);
+}
 
+
+ }); 
+
+var file= header+ form; 
+res.send(file);
 });
 
-
+// To post the entered details of user at the time of registration by clicking "submit" button.
 app.post('/registration', (req, res)=>{
-	if(req.body.username && req.body.email)
-	{
-		var user ={
-			username : req.body.username,
-			email : req.body.email,
-			dob :req.body.dob,
-			mobile :req.body.mobile, 
-			gender : req.body.gender,
-			dp :req.body.dp
+if(req.body.username && req.body.email)
+{
+var user ={
+username : req.body.username,
+email : req.body.email,
+dob :req.body.dob,
+mobile :req.body.mobile, 
+dp :req.body.dp
 
-		}
-	//console.log(user)
-
+}
      global.USERS.push(user);
      res.send('Record is inserted');
     }
 
    else
    {
-   	res.send("Please fill the details");
-   	// alert("Please fill all the required fields."); 
-   } 
+    res.send("Please fill the details");
+    } 
 });
 
-
-app.post('/userList/update', (req, res)=>{
+// To post the users updated profile info on click of "update" button
+app.post('/userList/:eid/update', (req, res)=>{
   
   console.log(req.body.username);
- 
-    var user ={ username : req.body.username,
-      email : req.body.email,
-      dob :req.body.dob,
-      mobile :req.body.mobile, 
-      gender : req.body.gender,
-      dp :req.body.dp }
-     
+   var userEmail = req.params.eid;
+   USERS.forEach(function(e){
+   if(userEmail == e.email)
+   {
+      e.username = req.body.username,
+      e.email = req.body.email,
+      e.dob =req.body.dob,
+      e.mobile =req.body.mobile, 
+      e.dp =req.body.dp 
+    }
+  });
+
       res.send("record updated successfully!!");
     
     
     
 });
 
+// To delete user from the user list
+app.delete('/userList/:eid/delete', (req, res)=>{
+var userEmail = req.params.eid; 
+var i=0;
+var j;
+
+USERS.forEach(function(e){
+  if(userEmail == e.email)
+  {
+    j=i;
+ 
+}
+i++;
+});
+ 
+ global.USERS.splice(j,1); 
+});
+ 
 //app.get('/usersListManagement', (req, res)=> {
    // res.send(JSON.stringify(global.USERS, null, '2'));
 
